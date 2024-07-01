2e10d9600293cf13e24cad9ad2ff2c45fa06d184
Andreas Handel <andreas.handel@gmail.com>
2024-07-01 15:40:21 -0400

    sismid presentations

--- makeredirects.R
+++ makeredirects.R
@@ -1,5 +1,5 @@
 # Based on: https://blog.djnavarro.net/posts/2022-04-20_porting-to-quarto/#netlify-redirects
 # lines to insert to a netlify _redirect file
-redirects <- paste0("/talk /presentations \n/talks /presentations \n/post /posts")
+redirects <- paste0("/talk /presentations \n/talks /presentations \n/presentations/ /presentations \n/post /posts")
 # write the _redirect file
 writeLines(redirects, here::here("_site", "_redirects"))
